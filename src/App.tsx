7820e3806fa28d4831ba9db43ad8976c3f5212b0
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2022-06-04 14:07:23 -0400

    DRT 2.4.3/4 - Styled Components - animation, props e theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,97 @@
-import styled from "styled-components";
+import styled, { css, keyframes, ThemeProvider } from "styled-components";
+import { useState } from "react";
 
 const StyledHeader = styled.h1`
-  color: #3636e9;
+  color: ${(props) => props.theme.main};
   font-size: 42px;
 `;
 
 const StyledData = styled("p")`
   font-size: 24px;
   font-weight: 700;
-  color: #8d8dff;
+  color: ${(props) => props.theme.seconday};
 `;
 
-const StyledButton = styled.button`
+type TStyledButtonProps = {
+  variant?: "success" | "failed";
+};
+
+const RotateKeyFrame = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+const themeBlue = {
+  main: "blue",
+  seconday: "#3636e9",
+};
+
+const themeRed = {
+  main: "red",
+  seconday: "#ff4343",
+};
+
+const StyledButton = styled.button<TStyledButtonProps>`
   background-color: transparent;
   border-radius: 8px;
   border: 1px solid black;
   padding: 8px 16px;
-`;
 
-const StyledSuccessButton = styled(StyledButton)`
-  border-color: green;
-  color: green;
-`;
+  :hover {
+    cursor: pointer;
+    background-color: #f2f2f2;
+  }
 
-const StyledFailedButton = styled(StyledButton)`
-  border-color: red;
-  color: red;
+  ${(props) => {
+    if (props.variant === "success") {
+      return css`
+        border-color: green;
+        color: green;
+      `;
+    }
+
+    if (props.variant === "failed") {
+      return css`
+        border-color: red;
+        color: red;
+      `;
+    }
+  }};
 `;
 
 function App() {
+  const [theme, setTheme] = useState(themeBlue);
   return (
     <div>
-      <StyledHeader>Rodrigo Borba</StyledHeader>
-      <StyledData>rodrigo@borba.com</StyledData>
-      <StyledData>81-99999-9999</StyledData>
-      <StyledData>Brasil</StyledData>
+      <ThemeProvider theme={theme}>
+        <StyledHeader>Rodrigo Borba</StyledHeader>
+        <div>
+          <StyledButton onClick={() => setTheme(themeBlue)}>
+            Set Blue theme
+          </StyledButton>
+          <StyledButton
+            style={{ marginLeft: "8px" }}
+            onClick={() => setTheme(themeRed)}
+          >
+            Set Red theme
+          </StyledButton>
+        </div>
+        <StyledData>rodrigo@borba.com</StyledData>
+        <StyledData>81-99999-9999</StyledData>
+        <StyledData>Brasil</StyledData>
 
-      <StyledSuccessButton>Adicionar</StyledSuccessButton>
-      <StyledFailedButton>Remover</StyledFailedButton>
+        <StyledButton variant="success">Adicionar</StyledButton>
+        <StyledButton variant="failed" style={{ marginLeft: "8px" }}>
+          Remover
+        </StyledButton>
 
-      <StyledButton>Detalhes</StyledButton>
+        <StyledButton style={{ marginLeft: "8px" }}>Detalhes</StyledButton>
+      </ThemeProvider>
     </div>
   );
 }
