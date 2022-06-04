9cd80a4017d5f793c36090c1305c722ca124f09d
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2022-06-04 13:23:44 -0400

    DRT 2.4.1 - Styled Components - instalacao - uso basico - extensao de elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.h1`
+  color: #3636e9;
+  font-size: 42px;
+`;
+
+const StyledData = styled("p")`
+  font-size: 24px;
+  font-weight: 700;
+  color: #8d8dff;
+`;
+
+const StyledButton = styled.button`
+  background-color: transparent;
+  border-radius: 8px;
+  border: 1px solid black;
+  padding: 8px 16px;
+`;
+
+const StyledSuccessButton = styled(StyledButton)`
+  border-color: green;
+  color: green;
+`;
+
+const StyledFailedButton = styled(StyledButton)`
+  border-color: red;
+  color: red;
+`;
+
 function App() {
-  return <div>Modulo 4</div>;
+  return (
+    <div>
+      <StyledHeader>Rodrigo Borba</StyledHeader>
+      <StyledData>rodrigo@borba.com</StyledData>
+      <StyledData>81-99999-9999</StyledData>
+      <StyledData>Brasil</StyledData>
+
+      <StyledSuccessButton>Adicionar</StyledSuccessButton>
+      <StyledFailedButton>Remover</StyledFailedButton>
+
+      <StyledButton>Detalhes</StyledButton>
+    </div>
+  );
 }
 
 export default App;
